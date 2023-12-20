b86ebe92467639557facac75bd020bb65e38e84b
icochet <icochet@etudiant.univ-rennes1.fr>
2023-12-20 14:14:19 +0100

    Test d'écriture dans un fichier html avec une chaine explode puis implode

--- Rendu2/user_documentation_generator/gendoc-user.php
+++ Rendu2/user_documentation_generator/gendoc-user.php
@@ -1,7 +1,9 @@
 #!/usr/bin/php
 
 <?php
-    $content = "Ceci est un test d'écriture dans un fichier html";
+    $content = "Ceci est un test:d'écriture dans un fichier html.";
+    $content_tab = explode(":", $content);
+    $content_reformer = implode("\n", $content_tab);
 
-    file_put_contents("doc-user-1.0.0.html", &content);
+    file_put_contents("doc-user-1.0.0.html", $content_reformer);
 ?>
