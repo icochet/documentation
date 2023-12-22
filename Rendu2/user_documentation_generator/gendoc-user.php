0d7a0df6efc53fa8f30e274675cd5e0e8f8860c5
icochet <icochet@etudiant.univ-rennes1.fr>
2023-12-22 10:14:32 +0100

    Les \n sont supprimés de chaque ligne du tableau

--- Rendu2/user_documentation_generator/gendoc-user.php
+++ Rendu2/user_documentation_generator/gendoc-user.php
@@ -3,19 +3,28 @@
 <?php
     $lines = file("doc.md");
 
-    
-
-    $content_md = implode("\n", $lines);
-
-    file_put_contents("doc-user-1.0.0.html", $content_md);
+    foreach ($lines as $numLine => $line) {
+        $lines[$numLine] = rtrim($line);
+    }
 ?>
-
 <!DOCTYPE html>
+
 <html lang="fr">
 <head>
     <title>Générateur de documentation utilisateur</title>
 </head>
 <body>
-    
+    <?php
+        foreach ($lines as $numLine => $line) {
+    ?>
+
+    <?php
+        }
+    ?>
 </body>
 </html>
+<?php
+    $content_md = implode("\n", $lines);
+
+    file_put_contents("doc-user-1.0.0.html", $content_md);
+?>
