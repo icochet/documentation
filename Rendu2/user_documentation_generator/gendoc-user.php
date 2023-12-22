9a4129a845b7b8ba796ff57d488e04292a4134c1
icochet <icochet@etudiant.univ-rennes1.fr>
2023-12-22 09:38:14 +0100

    Ajout de l'HTML

--- Rendu2/user_documentation_generator/gendoc-user.php
+++ Rendu2/user_documentation_generator/gendoc-user.php
@@ -3,7 +3,19 @@
 <?php
     $lines = file("doc.md");
 
+    
+
     $content_md = implode("\n", $lines);
 
     file_put_contents("doc-user-1.0.0.html", $content_md);
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Générateur de documentation utilisateur</title>
+</head>
+<body>
+    
+</body>
+</html>
